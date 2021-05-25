298edd1d871ecc6d262217d2ef696cfe25da841f
Mella Aulia Agusty <agustymella@gmail.com>
2021-05-25 19:53:23 +0700

    Add typeface, dependency for neumorphism, custom bottom navigation

---
+++ app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/MainActivity.kt
@@ -0,0 +1,14 @@
+package sidev.app.bangkit.capstone.sheltermobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}

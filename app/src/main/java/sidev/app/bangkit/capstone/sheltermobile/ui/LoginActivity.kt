64518f9753a57a88e4c3d79934d256751cbb8536
Mella Aulia Agusty <agustymella@gmail.com>
2021-05-29 11:32:49 +0700

    Article/News xml added

--- app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/LoginActivity.kt
+++ app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/LoginActivity.kt
@@ -1,11 +1,20 @@
 package sidev.app.bangkit.capstone.sheltermobile.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sidev.app.bangkit.capstone.sheltermobile.R
+import sidev.app.bangkit.capstone.sheltermobile.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
+
 }

8ac1a16000abb335b0bece6de5c35d61fd923c43
Mella Aulia Agusty <agustymella@gmail.com>
2021-05-30 20:42:40 +0700

    Add camera access for LaporPesanFragment.kt

--- app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/LaporPesanFragment.kt
+++ app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/LaporPesanFragment.kt
@@ -1,40 +1,94 @@
 package sidev.app.bangkit.capstone.sheltermobile.ui
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.jetbrains.anko.support.v4.startActivityForResult
 import sidev.app.bangkit.capstone.sheltermobile.R
+import sidev.app.bangkit.capstone.sheltermobile.databinding.FragmentLaporPesanBinding
 
 
+@Suppress("DEPRECATION")
 class LaporPesanFragment : Fragment() {
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
+
+    private lateinit var binding : FragmentLaporPesanBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lapor_pesan, container, false)
+    ): View {
+        binding = FragmentLaporPesanBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LaporPesanFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LaporPesanFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //call the function
+        cameraFuction()
     }
+
+    private fun cameraFuction() {
+        binding.attachPhoto.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+            ){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                ActivityCompat.requestPermissions((
+                        requireActivity()),
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_CODE
+                )
+            }
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_REQUEST_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            } else {
+                Toast.makeText(context,"Ups anda menolak Shelter menggunakan kamera anda", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == CAMERA_REQUEST_CODE){
+                val thumbNail: Bitmap = data?.extras?.get("data") as Bitmap //data can be from any type but we convert it to Bitmap hehe :p
+                binding.ivImage.setImageBitmap(thumbNail)
+            }
+        }
+    }
+
+
 }

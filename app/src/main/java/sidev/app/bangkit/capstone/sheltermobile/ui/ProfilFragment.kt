1145980ae56be73cc96b2536dd890e0ea5a30bc4
Mella Aulia Agusty <agustymella@gmail.com>
2021-06-01 20:15:10 +0700

    Working on ProfilFragment.kt

--- app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/ProfilFragment.kt
+++ app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/ProfilFragment.kt
@@ -35,7 +35,7 @@ class PengaturanFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pengaturan, container, false)
+        return inflater.inflate(R.layout.fragment_profil, container, false)
     }
 
     companion object {

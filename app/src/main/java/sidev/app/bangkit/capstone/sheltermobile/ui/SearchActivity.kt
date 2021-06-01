11eabf9a254d2cdecd1a5c85ecb6027951933797
Mella Aulia Agusty <agustymella@gmail.com>
2021-06-01 23:46:05 +0700

    Working on Literatur Activity

---
+++ app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/SearchActivity.kt
@@ -0,0 +1,12 @@
+package sidev.app.bangkit.capstone.sheltermobile.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sidev.app.bangkit.capstone.sheltermobile.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}

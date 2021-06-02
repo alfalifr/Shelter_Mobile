77877f71e5f96c170635c1fa368bc7333372017d
Mella Aulia Agusty <agustymella@gmail.com>
2021-06-02 17:43:19 +0700

    Working on Detil Lapor

--- app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/PengaturanEditActivity.kt
+++ app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/PengaturanEditActivity.kt
@@ -1,12 +1,36 @@
 package sidev.app.bangkit.capstone.sheltermobile.ui
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import sidev.app.bangkit.capstone.sheltermobile.R
+import sidev.app.bangkit.capstone.sheltermobile.databinding.ActivityPengaturanEditBinding
 
 class PengaturanEditActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityPengaturanEditBinding
+
+    private lateinit var btn : Button
+    internal lateinit var myDialog : Dialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pengaturan_edit)
+        binding = ActivityPengaturanEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //call function
+        dialogueLocation()
+    }
+
+    private fun dialogueLocation() {
+        btn = binding.tvChangeLocation as Button
+        btn.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+
     }
 }
---
+++ app/src/main/java/sidev/app/bangkit/capstone/sheltermobile/ui/RiwayatLaporDetilActivity.kt
@@ -0,0 +1,12 @@
+package sidev.app.bangkit.capstone.sheltermobile.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sidev.app.bangkit.capstone.sheltermobile.R
+
+class RiwayatLaporDetilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_riwayat_lapor_detil)
+    }
+}
